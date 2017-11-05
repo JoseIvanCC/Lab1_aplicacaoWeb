a604e7b14e9c6aa0a04a047786c38808b6efb83c
JoseIvanCC <jose.ivan.junior@ccc.ufcg.edu.br>
2017-11-05 17:53:42 -0300

    Cricao do objeto música e álbum, além da modificação para tornar possível a adição de músicas

--- controller.js
+++ controller.js
@@ -1,8 +1,36 @@
 angular.module("Scgfy", []);
-        angular.module("Scgfy").controller("ScgfyCtrl", function ($scope) {
+angular.module("Scgfy").controller("ScgfyCtrl", function ($scope) {
             $scope.app = "Scgfy";
             $scope.artistas = [];
+            $scope.musicas = [];
 
+function Album(nomeAlbum, autorAlbum) {
+    this.musicas = [];
+    this.nomeAlbum = nomeAlbum;
+    this.autorAlbum = autorAlbum;
+}
+
+const imprimir = function () {
+  alert("oi");
+}
+
+$scope.adicionarMusica = function (music) {
+    if (existeMusica(this.musicas, music)) {
+        alert("Música já existente no álbum");
+  } else {
+        $scope.musicas.push(angular.copy(music));
+        delete $scope.musica;
+  }
+}
+
+const existeMusica = function (musicas, nomeMusica) {
+    for (i = 0; i < musicas.length; i++) {
+        if (musicas[i].nome ===  nomeMusica) {
+            return true;
+        }
+    }
+          return false;
+}
 
             /*var album = function(musicas: [], nome) {
               this.musicas = musicas;
@@ -27,21 +55,22 @@ angular.module("Scgfy", []);
                 return false;
             }*/
 
-            $scope.adicionarArtista = function (artist) {
-                if (existeArtista(artist)) {
-                  alert("artista já existente no sistema");
-                } else {
-                  $scope.artistas.push(angular.copy(artist));
-                  delete $scope.artista;
-                }
-            };
+$scope.adicionarArtista = function (artist) {
+    if (existeArtista(artist)) {
+        alert("artista já existente no sistema");
+            } else {
+                $scope.artistas.push(angular.copy(artist));
+                    delete $scope.artista;
+                    }
+                      };
 
-            const existeArtista = function(artist) {
-                for (i = 0; i < $scope.artistas.length; i++) {
-                      if (artist.nome === $scope.artistas[i].nome) {
-                        return true;
+const existeArtista = function(artist) {
+    for (i = 0; i < $scope.artistas.length; i++) {
+          if (artist.nome === $scope.artistas[i].nome) {
+            return true;
                       }
                   }
                   return false;
                 };
+
         });
